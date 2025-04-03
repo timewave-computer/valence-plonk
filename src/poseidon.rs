619e17d6a28296e2aca6221c06430213a84fae02
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2025-04-03 15:19:02 +0200

    serialize elements as bigints

--- src/poseidon.rs
+++ src/poseidon.rs
@@ -1,102 +1,119 @@
-use plonky2::util::serialization::IoResult;
-use plonky2::plonk::config::GenericConfig;
-use serde_json::json;
-use std::fs::File;
-use std::io::Write;
+use plonky2::hash::poseidon::PoseidonHash;
+use plonky2::iop::witness::{PartialWitness, WitnessWrite};
+use plonky2::plonk::circuit_builder::CircuitBuilder;
+use plonky2::plonk::circuit_data::{CircuitConfig, VerifierCircuitData};
+use plonky2::util::serialization::{DefaultGateSerializer, IoResult};
+use plonky2::plonk::config::{GenericConfig, Hasher, PoseidonGoldilocksConfig};
+use serde_json::Value;
+use std::fs;
+use serde::Serialize;
 
+const D: usize = 2;
+type C = PoseidonGoldilocksConfig;
+type F = <C as GenericConfig<D>>::F;
 
-fn save_files<C: GenericConfig<D>, const D: usize>(
-    // todo: fix this serialization
+/// Turn arrays of field elements under "elements" into a single string bigint
+fn serialize_elements_as_bigint(val: Value) -> Value {
+    match val {
+        Value::Object(mut map) => {
+            if let Some(elements) = map.remove("elements") {
+                if let Value::Array(arr) = elements {
+                    let joined = arr
+                        .into_iter()
+                        .map(|v| v.to_string())
+                        .collect::<Vec<_>>()
+                        .join(""); // join digits
+                    return Value::String(joined);
+                }
+            }
+            let keys: Vec<String> = map.keys().cloned().collect();
+            for k in keys {
+                if let Some(v) = map.remove(&k) {
+                    map.insert(k, serialize_elements_as_bigint(v));
+                }
+            }
+            Value::Object(map)
+        }
+        Value::Array(vec) => {
+            Value::Array(vec.into_iter().map(serialize_elements_as_bigint).collect())
+        }
+        other => other,
+    }
+}
+
+pub(crate) fn save_files<C: GenericConfig<D>, const D: usize>(
     data: &plonky2::plonk::circuit_data::CircuitData<C::F, C, D>,
     proof: &plonky2::plonk::proof::ProofWithPublicInputs<C::F, C, D>,
-) -> IoResult<()> {
-    // Serialize CommonCircuitData
-    let common_data_bytes = serde_json::to_string_pretty(&data.common).unwrap();
-    let mut common_data_file = File::create("src/out/common_circuit_data.json").unwrap();
-    common_data_file.write_all(common_data_bytes.as_bytes()).unwrap();
+) -> IoResult<()>
+where
+    C: Serialize,
+{
+    let common_data_json = serde_json::to_value(&data.common).unwrap();
+    let verifier_only_json = serde_json::to_value(&data.verifier_only).unwrap();
+    let proof_json = serde_json::to_value(&proof).unwrap();
 
-    // Serialize VerifierOnlyCircuitData (use DefaultGateSerializer)
-    let verifier_only_json = json!({
-        "circuit_digest": data.verifier_only.circuit_digest,
-        "constants_sigmas_cap": data.verifier_only.constants_sigmas_cap,
-    });    
-    let verifier_data_json = serde_json::to_string_pretty(&verifier_only_json).unwrap();
-    println!("verifier_data: {:?}", data.verifier_only);
-    let mut verifier_data_file = File::create("src/out/verifier_only_circuit_data.json").unwrap();
-    verifier_data_file.write_all(verifier_data_json.as_bytes()).unwrap();
+    let converted_common = serialize_elements_as_bigint(common_data_json);
+    let converted_verifier = serialize_elements_as_bigint(verifier_only_json);
+    let converted_proof = serialize_elements_as_bigint(proof_json);
 
-    // Serialize ProofWithPublicInputs
-    let proof_bytes = serde_json::to_string_pretty(proof).unwrap();
-    println!("proof: {:?}", proof);
-    let mut proof_file = File::create("src/out/proof_with_public_inputs.json").unwrap();
-    proof_file.write_all(proof_bytes.as_bytes()).unwrap();
+    fs::write(
+        "src/out/common_circuit_data.json",
+        serde_json::to_string_pretty(&converted_common).unwrap(),
+    ).unwrap();
+
+    fs::write(
+        "src/out/verifier_only_circuit_data.json",
+        serde_json::to_string_pretty(&converted_verifier).unwrap(),
+    ).unwrap();
+
+    fs::write(
+        "src/out/proof_with_public_inputs.json",
+        serde_json::to_string_pretty(&converted_proof).unwrap(),
+    ).unwrap();
 
     Ok(())
 }
 
-
-
-#[cfg(test)]
-mod examples {
-    use super::save_files;
-    use plonky2::util::serialization::DefaultGateSerializer;
+#[test]
+fn poseidon_flat_public_inputs() -> anyhow::Result<()> {
     use plonky2::field::types::Field;
-    use plonky2::hash::poseidon::PoseidonHash;
-    use plonky2::iop::witness::{PartialWitness, WitnessWrite};
-    use plonky2::plonk::circuit_builder::CircuitBuilder;
-    use plonky2::plonk::circuit_data::{CircuitConfig, VerifierCircuitData};
-    use plonky2::plonk::config::{GenericConfig, Hasher, PoseidonGoldilocksConfig};
-    #[test]
-    fn poseidon() -> anyhow::Result<()> {
-        const D: usize = 2;
-        type C = PoseidonGoldilocksConfig;
-        type F = <C as GenericConfig<D>>::F;
 
-        let config = CircuitConfig::standard_recursion_config();
-        let mut builder = CircuitBuilder::<F, D>::new(config);
+    let config = CircuitConfig::standard_recursion_config();
+    let mut builder = CircuitBuilder::<F, D>::new(config);
 
-        let preimage = builder.add_virtual_target();
+    let preimage = builder.add_virtual_target();
+    let expected_hash = builder.add_virtual_hash();
 
-        let expected_hash_1 = builder.add_virtual_target();
-        let expected_hash_2 = builder.add_virtual_target();
-        let expected_hash_3 = builder.add_virtual_target();
-        let expected_hash_4 = builder.add_virtual_target();
+    let inputs = vec![preimage];
+    let hash_target = builder.hash_n_to_hash_no_pad::<PoseidonHash>(inputs);
 
-        builder.register_public_input(expected_hash_1);
-        builder.register_public_input(expected_hash_2);
-        builder.register_public_input(expected_hash_3);
-        builder.register_public_input(expected_hash_4);
-
-        let inputs = vec![preimage];
-        let hash_target = builder.hash_n_to_hash_no_pad::<PoseidonHash>(inputs);
-
-        builder.connect(hash_target.elements[0], expected_hash_1);
-        builder.connect(hash_target.elements[1], expected_hash_2);
-        builder.connect(hash_target.elements[2], expected_hash_3);
-        builder.connect(hash_target.elements[3], expected_hash_4);
-
-        let data = builder.build::<C>();
-        let preimage_value = F::from_canonical_u64(7);
-        let hash_val = PoseidonHash::hash_no_pad(&[preimage_value]);
-
-        let mut pw = PartialWitness::new();
-        // set the targets for the expected, constrained hash values
-        pw.set_target(preimage, preimage_value)?;
-        pw.set_target(expected_hash_1, hash_val.elements[0])?;
-        pw.set_target(expected_hash_2, hash_val.elements[1])?;
-        pw.set_target(expected_hash_3, hash_val.elements[2])?;
-        pw.set_target(expected_hash_4, hash_val.elements[3])?;
-
-        let proof = data.prove(pw)?;
-        save_files(&data, &proof).expect("Failed to write proof files");
-        println!("Expected hash: {:?}", hash_val.elements.to_vec());
-        println!("Proof public input: {:?}", proof.public_inputs.to_vec());
-        let elf_serialized = data.verifier_data().to_bytes(&DefaultGateSerializer).expect("Failed to serialize program ELF");
-        let elf_deserialized: VerifierCircuitData<F, C, D> = VerifierCircuitData::from_bytes(elf_serialized, &DefaultGateSerializer).expect("Failed to deserialize program ELF");
-        elf_deserialized.verify(proof)?;
-        Ok(())
+    for e in expected_hash.elements {
+        builder.register_public_input(e);
     }
 
-    // to hash poseidon hashs together in a merkle tree:
-    // parent = PoseidonHash::hash_no_pad(&[...left.elements, ...right.elements])
+    builder.connect_hashes(expected_hash, hash_target);
+
+    let data = builder.build::<C>();
+
+    let preimage_value = F::from_canonical_u64(7);
+    let hash_val = PoseidonHash::hash_no_pad(&[preimage_value]);
+
+    let mut pw = PartialWitness::new();
+    pw.set_target(preimage, preimage_value);
+    pw.set_hash_target(expected_hash, hash_val);
+
+    let proof = data.prove(pw)?;
+
+    save_files(&data, &proof).unwrap();
+
+    let elf_serialized = data.verifier_data().to_bytes(&DefaultGateSerializer)
+        .expect("Failed to serialize program ELF");
+
+    let elf_deserialized: VerifierCircuitData<F, C, D> = VerifierCircuitData::from_bytes(
+        elf_serialized,
+        &DefaultGateSerializer,
+    ).expect("Failed to deserialize program ELF");
+
+    elf_deserialized.verify(proof)?;
+    Ok(())
 }
